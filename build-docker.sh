0d2a05a147c8e2c1087be83307b8a146ecda927c
Jesper Lloyd <jpl.lloyd@gmail.com>
2019-08-31 22:04:18 +0200

    Replace on-docker build script
    Rename build-docker to build-on-docker to not confuse its
    function with the building of docker images.
    
    Expand the script to check for the presence of
    all necessary repositories and accept alternative
    docker build images through an argument

--- build-docker.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#rm -rf RawTherapee
-if [ ! -e mypaint ]; then
-	git clone https://github.com/mypaint/mypaint.git
-fi
-#docker run -it -v $(pwd):/sources -e "RT_BRANCH=$RT_BRANCH" photoflow/docker-centos7-gtk bash
-docker run -it -v $(pwd):/sources centos:7 bash #/sources/ci/appimage-centos7.sh
-
---
+++ build-on-docker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTDIR=$(dirname $(readlink -f "$0"))
+APPIM_INIT_SCRIPT="mkappimage.sh"
+
+# If provided, the first argument should be the name
+# of the docker image that the build will be run on
+DOCKER_IMAGE="centos:7"
+if [ -n "$1" ]; then
+    DOCKER_IMAGE="$1"
+fi
+
+function check_prerequisites
+{
+    local FAILED=0
+    local GH_LOC="https://github.com/mypaint"
+    for dir in $@; do
+	# If the repo directory is not present: issue warning, set failure flag
+        if [ ! -e "$SCRIPTDIR/$dir" ]; then
+	    echo "============================================================"
+	    echo "Project directory \"$dir\" not in $SCRIPTDIR, fetch it with:"
+	    # Make the command easier to copy/paste by making it a
+	    # one-liner even when running from another directory
+	    local DST=""
+	    if [ "$(readlink -f $(pwd))" != "$SCRIPTDIR" ]; then
+		DST=" $SCRIPTDIR/$dir"
+	    fi
+	    echo "git clone --depth=1 $GH_LOC/$dir"".git""$DST"
+	    echo "============================================================"
+	    FAILED=1
+	fi
+    done
+    if [ $FAILED != 0 ]; then
+	echo "Cancelling build, required files are missing (see above)."
+    fi
+    return $FAILED
+}
+
+# Check whether all required external repositories are present
+check_prerequisites mypaint libmypaint mypaint-brushes
+
+# If the check passes, start the appimage build in a docker container
+docker run -it -v ${SCRIPTDIR}:/sources "$DOCKER_IMAGE" "sources/$APPIM_INIT_SCRIPT"
+
