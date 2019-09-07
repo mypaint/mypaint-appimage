248158674627869e1538f781c21fc26a2070d178
Jesper Lloyd <jpl.lloyd@gmail.com>
2019-09-07 20:11:03 +0200

    Fix/refactor bundling scripts
    Remove parts which are not relevant to gtk3 >= 3.14 (theme engines),
    unused parts (pango cache) and commented-out instructions.
    
    Use regular posix tests instead of x$... fallbacks and refactor
    the "exit 1" chain by using "set -e" for the scripts instead.

--- scripts/helpers/bundle-python.sh
+++ scripts/helpers/bundle-python.sh
@@ -1,39 +1,46 @@
-#! /bin/bash
+#!/bin/bash
 
 # Bundle the python runtime
 PYTHON_PREFIX=$(pkg-config --variable=prefix python)
 PYTHON_LIBDIR=$(pkg-config --variable=libdir python)
 PYTHON_VERSION=$(pkg-config --modversion python)
-if [ x"${PYTHON_PREFIX}" = "x" ]; then
+if [ -z "${PYTHON_PREFIX}" ]; then
 	echo "Could not determine PYTHON installation prefix, exiting."
 	exit 1
 fi
-if [ x"${PYTHON_LIBDIR}" = "x" ]; then
+if [ -z "${PYTHON_LIBDIR}" ]; then
 	echo "Could not determine PYTHON library path, exiting."
 	exit 1
 fi
-if [ x"${PYTHON_VERSION}" = "x" ]; then
+if [ -z "${PYTHON_VERSION}" ]; then
 	echo "Could not determine PYTHON version, exiting."
 	exit 1
 fi
-cp -a "${PYTHON_PREFIX}/bin"/python* "$APPDIR/usr/bin" || exit 1
+
+set -e
+
+cp -a "${PYTHON_PREFIX}/bin"/python* "$APPDIR/usr/bin"
 rm -rf "$APPDIR/usr/lib/python${PYTHON_VERSION}"
 mkdir -p "$APPDIR/usr/lib"
-cp -a "${PYTHON_LIBDIR}/python${PYTHON_VERSION}" "$APPDIR/usr/lib" || exit 1
-PYGLIB_LIBDIR=$(pkg-config --variable=libdir pygobject-2.0)
+cp -a "${PYTHON_LIBDIR}/python${PYTHON_VERSION}" "$APPDIR/usr/lib"
+
+PYGLIB_LIBDIR=$(pkg-config --variable=libdir pygobject-2.0 || true)
 if [ x"${PYGLIB_LIBDIR}" != "x" ]; then
 	cp -a "${PYGLIB_LIBDIR}"/libpyglib*.so* "$APPDIR/usr/lib"
 else
 	echo "Could not determine PYGOBJECT-2.0 library path."
 fi
-(cd "$APPDIR/usr" && mkdir -p lib64 && cd lib64 && rm -rf python${PYTHON_VERSION} && ln -s ../lib/python${PYTHON_VERSION} .) || exit 1
-ls -l "$APPDIR/usr/lib64"
 
-
+mkdir -p "$APPDIR/usr/lib64"
+cd "$APPDIR/usr/lib64"
+rm -rf python${PYTHON_VERSION}
+ln -s ../lib/python${PYTHON_VERSION} .
+cd -
 
 gssapilib=$(ldconfig -p | grep 'libgssapi_krb5.so.2 (libc6,x86-64)'| awk 'NR==1{print $NF}')
-if [ x"$gssapilib" != "x" ]; then
+if [ -n "$gssapilib" ]; then
 	gssapilibdir=$(dirname "$gssapilib")
 	cp -a "$gssapilibdir"/libgssapi_krb5*.so* "$APPDIR/usr/lib"
 fi
 
+echo "Python bundling finished"
