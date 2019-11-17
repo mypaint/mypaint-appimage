bddea440e6ab6088d0ec809a4a338fd2c1425aba
Jesper Lloyd <jpl.lloyd@gmail.com>
2019-11-17 13:46:48 +0100

    Use swig3 in docker build image
    Bump image version to 1.1.0

--- build-on-docker.sh
+++ build-on-docker.sh
@@ -7,7 +7,7 @@ APPIM_INIT_SCRIPT="scripts/mkappimage.sh"
 
 # If provided, the first argument should be the name
 # of the docker image that the build will be run on
-DOCKER_IMAGE="mypaint/appimage-base:1.0.0"
+DOCKER_IMAGE="mypaint/appimage-base:1.1.0"
 if [ -n "$1" ]; then
     DOCKER_IMAGE="$1"
 fi
